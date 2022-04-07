141585939c6607fb72ffccef8e197e718a1f440d
kostiya <kostiya@users.noreply.github.com>
2022-04-07 10:44:38 +0300

    Ex 3.1-3.2: GET id and info screen

---
+++ index.js
@@ -0,0 +1,50 @@
+const { response } = require("express")
+const express = require("express")
+const app = express()
+
+let persons = 
+[
+    { 
+      "id": 1,
+      "name": "Arto Hellas", 
+      "number": "040-123456"
+    },
+    { 
+      "id": 2,
+      "name": "Ada Lovelace", 
+      "number": "39-44-5323523"
+    },
+    { 
+      "id": 3,
+      "name": "Dan Abramov", 
+      "number": "12-43-234345"
+    },
+    { 
+      "id": 4,
+      "name": "Mary Poppendieck", 
+      "number": "39-23-6423122"
+    }
+]
+
+app.get('/api/persons/:id', (request, response)=> {
+    const id = Number(request.params.id)
+    const person = persons.find( person => person.id === id)
+
+    if(person){
+        response.json(person)
+    } else {
+        response.statusMessage = 'No such person'
+        response.status(404).end()
+    }
+})
+
+app.get('/info', (request, response) => {
+    response.contentType('text/plain')
+    response.write(`Phonebook has info for ${persons.length} people\n`)
+    response.write(Date().toLocaleString())
+    response.end()
+})
+
+const PORT = 3001
+app.listen(PORT)
+console.log("Server running on port " + PORT)
