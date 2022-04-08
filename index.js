7ff001a0a5700f15f0f97be03db80d7464238b89
kostiya <kostiya@users.noreply.github.com>
2022-04-08 05:33:27 +0300

    Ex 3.4-3.5 Implemented DELETE and POST

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
+const { request } = require("express")
 const { response } = require("express")
 const express = require("express")
 const app = express()
+
+app.use(express.json())
 
 let persons = 
 [
@@ -47,6 +50,33 @@ app.get('/info', (request, response) => {
 
 app.get('/api/persons', (request, response) => {
     response.json(persons)
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  persons = persons.filter(person => person.id !== id)
+
+  response.status(204).end();
+})
+
+app.post('/api/persons', (request, response) => {
+  const body = request.body
+
+  if(!body.name){
+    return response.status(400).json({
+      error : 'Name is missing'
+    })
+  }
+
+  const newPerson = {
+    id : Math.round(Math.random()*1000000000),
+    name : body.name,
+    number : body.number ? body.number : "0"
+  }
+
+  persons = persons.concat(newPerson)
+
+  response.json(newPerson)
 })
 
 const PORT = 3001
