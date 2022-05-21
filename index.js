587173dd5b64bcd07ff25f39d317509508f30c7b
kostiya <kostiya@users.noreply.github.com>
2022-05-21 21:34:56 +0300

    Finished 3.16 - Error handler working

--- index.js
+++ index.js
@@ -44,10 +44,15 @@ let persons =
     }
 ]
 
-app.get('/api/persons/:id', (request, response)=> {
+app.get('/api/persons/:id', (request, response, next)=> {
     Person.findById(request.params.id).then(person => {
-      response.json(person)
+      if(person){
+        response.json(person)
+      } else {
+        response.status(404).end()
+      }
     })
+    .catch(error => next(error))
 })
 
 app.get('/info', (request, response) => {
@@ -63,12 +68,12 @@ app.get('/api/persons', (request, response) => {
     })
 })
 
-app.delete('/api/persons/:id', (request, response) => {
+app.delete('/api/persons/:id', (request, response, next) => {
     Person.findByIdAndRemove(request.params.id)
       .then(result => {
         response.status(204).end()
       })
-      .catch(error => console.log(error))
+      .catch(error => next(error))
 })
 
 app.post('/api/persons', (request, response) => {
@@ -95,6 +100,24 @@ app.post('/api/persons', (request, response) => {
     response.json(savedPerson)
   })
 })
+
+unknownEndpoint = (request, response) => {
+  response.status(404).send( {error : 'unknown endpoint'} )
+}
+
+app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if(error.name === "CastError") {
+    return response.status(400).send({ error: "malformated id" })
+  }
+
+  next(error)
+}
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT,() => {
