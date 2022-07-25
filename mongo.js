3c17e607def5a21eec042d66576a146fd4a1389a
kostiya <kostiya@users.noreply.github.com>
2022-07-25 19:11:35 +0300

    mono fixed

--- mongo.js
+++ mongo.js
@@ -1,9 +1,8 @@
-const res = require('express/lib/response')
 const mongoose = require('mongoose')
 
 if(process.argv.length < 3){
-    console.log("Please provide password as second argument")
-    process.exit(1)
+  console.log('Please provide password as second argument')
+  process.exit(1)
 }
 
 const password = process.argv[2]
@@ -13,29 +12,30 @@ const url = `mongodb+srv://kostiya:${password}@cluster0.7ejbv.mongodb.net/phoneB
 mongoose.connect(url)
 
 const personSchema = mongoose.Schema({
-    name: String,
-    number: String,
+  name: String,
+  number: String,
 })
 
 const Person = mongoose.model('Person', personSchema)
 
 if(process.argv.length > 3){
-    const person = new Person({
-        name: process.argv[3],
-        number: process.argv[4] || '',
-    })
+  const person = new Person({
+    name: process.argv[3],
+    number: process.argv[4] || '',
+  })
 
-    person.save().then(result => {
-        console.log('Person saved!')
-        mongoose.connection.close()
-        process.exit(0)
-    })
+  // eslint-disable-next-line no-unused-vars
+  person.save().then(_result => {
+    console.log('Person saved!')
+    mongoose.connection.close()
+    process.exit(0)
+  })
 }
 
 if(process.argv.length === 3){
-    Person.find({}).then(result => {
-        result.forEach(person => console.log(person.name, person.number))
-        mongoose.connection.close()
-        process.exit(0)
-    })
+  Person.find({}).then(result => {
+    result.forEach(person => console.log(person.name, person.number))
+    mongoose.connection.close()
+    process.exit(0)
+  })
 }
